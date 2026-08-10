504c51d78b82bda80936bcb5b0261c5b57282201
BergenCountyParents <info@bergencountyparents.com>
2026-08-10 13:14:17 -0400

    Refactor readableDate filter and add passthrough copies
    Added passthrough copy for CSS, images, and admin files.

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,22 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addFilter("readableDate", function (dateObj) {
-          const d = new Date(dateObj);
-          return d.toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric"
-          });
-    });
+      eleventyConfig.addFilter("readableDate", function (dateObj) {
+              const d = new Date(dateObj);
+              return d.toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric"
+              });
+      });
 
-    return {
-          dir: {
-                  input: "src",
-                  includes: "_includes",
-                  output: "_site"
-          }
-    };
+      eleventyConfig.addPassthroughCopy("src/css");
+      eleventyConfig.addPassthroughCopy("src/images");
+      eleventyConfig.addPassthroughCopy("src/admin");
+
+      return {
+              dir: {
+                        input: "src",
+                        includes: "_includes",
+                        output: "_site"
+              }
+      };
 };
